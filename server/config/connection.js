3ef0342acb3e64de53c010fca025e6821b9a92b2
Tim <tvanmookletcher@gmail.com>
2023-05-17 19:44:42 +0930

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bookapp');
 
 module.exports = mongoose.connection;
